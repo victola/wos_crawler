377c9a02c16883094b30a4de5a1863dcf07375d1
victola <victola.pz@gmail.com>
2019-03-21 18:25:47 +0800

    mixed mode

--- wos_crawler/main.py
+++ wos_crawler/main.py
@@ -1,4 +1,4 @@
-from gui.main_gui import *
+# from gui.main_gui import *
 import settings
 from scrapy import cmdline
 
@@ -16,6 +16,11 @@ def crawl_by_query(query, output_path='../output', document_type='Article', outp
             output_path, output_format).split() +
         ['-a', 'query={}'.format(query), '-a', 'document_type={}'.format(document_type)])
 
+def crawl_by_mixed_query(query, journal_list_path, output_path='../output', document_type='Article', output_format='bibtex'):
+    cmdline.execute(
+        r'scrapy crawl wos_mixed_query_spider -a output_path={} -a output_format={}'.format(
+            output_path, output_format).split() +
+        ['-a', 'query={}'.format(query), '-a', 'document_type={}'.format(document_type), '-a', 'journal_list_path={}'.format(journal_list_path)])
 
 def crawl_by_gui():
     gui_crawler = GuiCrawler()
@@ -24,6 +29,11 @@ def crawl_by_gui():
 
 
 if __name__ == '__main__':
+
+    journal_list_path=r'I:\Baidu Yun\Github\clone\wos_crawler\input\journal_list.txt'
+    query = 'TS=(warmth AND competence)'
+    query = 'TS=(recommendation agent)'
+
     # 按期刊下载
     # crawl_by_journal(journal_list_path=r'C:\Users\Tom\PycharmProjects\wos_crawler\input\journal_list_test.txt',
     #                  output_path='../output', output_format='bibtex', document_type='')
@@ -31,6 +41,12 @@ if __name__ == '__main__':
     # 按检索式下载
     # crawl_by_query(query='TS=information science AND PY=(2018)',
     #                output_path='../output', output_format='bibtex', document_type='proceedings paper')
+    crawl_by_query(query=query,
+                   output_path='../output', output_format='fieldtagged', document_type='')
+    
+    # 按检索和指定期刊下载
+    # crawl_by_mixed_query(query=query, journal_list_path=journal_list_path,
+    #                output_path='../output', output_format='fieldtagged', document_type='')
 
     # 使用GUI下载
     # crawl_by_gui()
